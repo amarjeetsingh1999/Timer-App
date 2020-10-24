b29f25768b55762a06934b7c11975dd40b67f2cf
Amarjeet Singh <amarjeetsingh1999@ymail.com>
2020-10-24 13:43:20 +0530

    Updates

---
+++ precache-manifest.35a5cdc0c52cd5b6b9eaa7010305b8d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c2b10bffa77c8ed85d70819b8ca0088",
+    "url": "/Timer-App/index.html"
+  },
+  {
+    "revision": "1e1cfca2e7fefd1ebc9a",
+    "url": "/Timer-App/static/css/main.55b3a100.chunk.css"
+  },
+  {
+    "revision": "900154ff442e2bd786a1",
+    "url": "/Timer-App/static/js/2.9515e3dc.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/Timer-App/static/js/2.9515e3dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e1cfca2e7fefd1ebc9a",
+    "url": "/Timer-App/static/js/main.81535941.chunk.js"
+  },
+  {
+    "revision": "d5047d6a3bf7cdf0a6b0",
+    "url": "/Timer-App/static/js/runtime-main.a0832574.js"
+  }
+]);
